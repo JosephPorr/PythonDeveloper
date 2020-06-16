b3a631377b9bd2aa335f73b3f8836c6fe1459df2
Joseph Porr <usmarine1978@gmail.com>
2020-06-15 20:33:40 -0400

    Create polymorphing.py

---
+++ polymorphing.py
@@ -0,0 +1,13 @@
+ #  Polymorphism in python defies methods in the child class rhar have the same name 
+ # as the methods in the parent class.
+
+ class  Car:
+     """
+     Car models a car w/ tires and an engine
+     """
+
+     def __init__(self, enginem tires):
+        self.engine = engine
+        self.tires = tires
+
+    def description(self):  # Self is used everytime we define a method.
