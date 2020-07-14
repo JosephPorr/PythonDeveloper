39900e26cbc6bdf64e9c3a01339f6bf3227e34f6
Joseph Porr <usmarine1978@gmail.com>
2020-07-13 21:35:04 -0400

    Create __init__.py

---
+++ argparserpackage/__init__.py
@@ -0,0 +1,6 @@
+# To create a package we need to have a directory with our package name 
+# that includes a __init__.py.  This is used as a placeholder for
+# the src.  The __init__.py files are required to make Python treat the 
+# directories as containing packages; this is done to prevent directories 
+# with a common name, such as string, from unintentionally hiding valid 
+# modules that occur later on the module search path.
