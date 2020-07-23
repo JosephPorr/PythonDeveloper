4abb47b5b2163ebb5bb29ccdcce11de13337373f
Joseph Porr <usmarine1978@gmail.com>
2020-07-22 20:55:19 -0400

    Update storage.py

--- argparserpackage/storage.py
+++ argparserpackage/storage.py
@@ -5,3 +5,5 @@ def local(infile, outfile):
     outfile.close()
     infile.close()  # streamed into file and closed
 
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
