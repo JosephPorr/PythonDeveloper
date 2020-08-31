52961e9b1ad00b7bd3cf08da421aa00021bee43d
Joseph Porr <usmarine1978@gmail.com>
2020-08-30 21:30:53 -0400

    Update models.py

--- Flask_SQLAlchemy/models.py
+++ Flask_SQLAlchemy/models.py
@@ -10,3 +10,13 @@ class User(db.Model): # Database conection
     password = db.Column(db.String(200))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    notes = db.relationship('Note', backref='author', lazy=True)  # Lazy means it won't fetch all the notes
+
+# User omitted
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # DOn't want notes that doesn't have a user.  There has to be a user to have a ID
