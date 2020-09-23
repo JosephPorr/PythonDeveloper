5d923c378e18beb57d8e267d411d423c3a79105a
Joseph Porr <usmarine1978@gmail.com>
2020-09-22 22:13:23 -0400

    Create input_function.py

---
+++ input_function.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")  # its good practive to use a colon and/or space.
+color = input("What is your favorite color? ")
+age = input("How old are you? ")
+# Works from the inside out, 
+# and whatever value is returned from input is going to be
+# the arguement passed into the int function
