474e6ba8ef3e72f4399d7ee964f7c1235a6604bb
Joseph Porr <usmarine1978@gmail.com>
2020-09-23 20:58:00 -0400

    Update input_function.py

--- input_function.py
+++ input_function.py
@@ -4,3 +4,12 @@ age = input("How old are you? ")
 # Works from the inside out, 
 # and whatever value is returned from input is going to be
 # the arguement passed into the int function
+# NAME is AGE years old and loves the COLOR.
+
+# print(name, end=" ")  # end will equal a space instead of a new line in REPL
+# print("is " + str(age) + " years old", end=" ")
+# print("and loves the color " + color + ".", end=" ")
+
+print(name, 'is', age, 'years old and loves the color', color + '.', sep=" ") 
+
+# A sep will print each argument separated by the sep character which is '' by default.
