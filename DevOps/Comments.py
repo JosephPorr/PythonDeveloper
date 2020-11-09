9bc1e78fe108f565991da765bf7e0cbd527546d4
Joseph Porr <usmarine1978@gmail.com>
2020-11-08 19:44:43 -0500

    Create Comments.py

---
+++ DevOps/Comments.py
@@ -0,0 +1,13 @@
+#!C:\\Users\\Joe\\AppData\\Local\\Programs\\Python\\Python38-32\\python.exe
+
+# This is a full line comment
+# Can comment each line
+
+print("hello, World!") # this is a trailing comment
+
+"""
+This is NOT a block comment
+This a multiline string
+"""
+#  Visual Studio w/ VIM, use Shift+I to insert
+#  Visual Studio w/ VIM, press ESC then wq or q!
