ef9208908844821b0aa3f1ad90e26cd45672fb34
Joseph Porr <usmarine1978@gmail.com>
2020-11-23 21:32:24 -0500

    Create seperator.py

---
+++ Python/seperator.py
@@ -0,0 +1,6 @@
+# Adding keyword argument
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# The print() function now uses a dash, 
+# instead of a space, to separate the outputted arguments.
