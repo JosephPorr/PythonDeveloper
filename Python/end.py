de50c99eead8f3a7ab6fd03704ec30e5d5838f5f
Joseph Porr <usmarine1978@gmail.com>
2020-11-23 21:20:49 -0500

    Create end.py

---
+++ Python/end.py
@@ -0,0 +1,5 @@
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+# The end keyword argument determines the characters the print() function sends to the output 
+# once it reaches the end of both its positional arguments.
