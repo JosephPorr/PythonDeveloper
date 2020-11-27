8678baa91829b4350f8a654c1a4368acdff53702
Joseph Porr <usmarine1978@gmail.com>
2020-11-26 22:29:14 -0500

    Create OctalHexNumbers.py

---
+++ Python/OctalHexNumbers.py
@@ -0,0 +1,7 @@
+print(0o123) # Will print a zero+o binary value 83.
+print(0x123) # Will print 291
+
+# If an integer number is preceded by an 0O or 0o prefix (zero-o), 
+# it will be treated as an octal value. 
+# This means that the number must contain digits 
+# taken from the [0..7] range only. Hex is [0..9]
