6cb73026bfea10885357745f8407a5b6c85f2d44
Joseph Porr <usmarine1978@gmail.com>
2020-12-07 18:56:35 -0500

    Create exponent.py

---
+++ Python/exponent.py
@@ -0,0 +1,10 @@
+# tthe speed of light, expressed in meters per second. Written directly it would look like this: 300000000
+# To avoid writing out so many zeros, physics textbooks use an abbreviated form, which you have probably already seen: 3 x 10^8
+
+3E8
+
+# The letter E (you can also use the lower-case letter e - it comes from the word exponent) 
+# is a concise record of the phrase times ten to the power of.
+
+# the exponent (the value after the E) has to be an integer;
+#the base (the value in front of the E) may be an intege
