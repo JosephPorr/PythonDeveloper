7db4e6bd25ed0d5a52760e21083cee00918b2976
Joseph Porr <usmarine1978@gmail.com>
2020-12-07 19:49:08 -0500

    Create boolean.py

---
+++ Python/boolean.py
@@ -0,0 +1,5 @@
+# Boolean algebra - a part of algebra which makes use of only two distinct values: True and False, denoted as 1 and 0.
+# This measures truthiness.
+
+print(True > False)  # True because the value 1 greater than 0
+print(True < False)  # False = 0 or no value, that is less than 
