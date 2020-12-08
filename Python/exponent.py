4b6cad1b4ef70c95762013e3380c3cc099b87cb7
Joseph Porr <usmarine1978@gmail.com>
2020-12-07 19:08:09 -0500

    Update exponent.py

--- Python/exponent.py
+++ Python/exponent.py
@@ -1,4 +1,4 @@
-# tthe speed of light, expressed in meters per second. Written directly it would look like this: 300000000
+# Tthe speed of light, expressed in meters per second. Written directly it would look like this: 300000000
 # To avoid writing out so many zeros, physics textbooks use an abbreviated form, which you have probably already seen: 3 x 10^8
 
 3E8
@@ -7,4 +7,4 @@
 # is a concise record of the phrase times ten to the power of.
 
 # the exponent (the value after the E) has to be an integer;
-#the base (the value in front of the E) may be an intege
+# the base (the value in front of the E) may be an integer
