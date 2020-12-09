ed726853a1dc78088b5fa6c37a68657475826b03
Joseph Porr <usmarine1978@gmail.com>
2020-12-08 22:04:59 -0500

    Create modulo.py

---
+++ Python/Arithmetic/modulo.py
@@ -0,0 +1,10 @@
+# The result of the operator is a remainder left after the integer division.
+# In other words, it's the value left over after dividing one value by another to produce an integer quotient.
+
+ print(14 % 4) 
+
+ # 14 / 4 is 3 with a remander of 2, like  3 X 4 = 12, then 14 - 12 = 2
+
+# 14 // 4 gives 3 → this is the integer quotient;
+# 3 * 4 gives 12 → as a result of quotient and divisor multiplication;
+# 14 - 12 gives 2 → this is the remainder.
