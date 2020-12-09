4df6111d292e9e63778d5af5048766e4e68662e2
Joseph Porr <usmarine1978@gmail.com>
2020-12-08 20:54:29 -0500

    Create exponentiation.py

---
+++ Python/Arithmetic/exponentiation.py
@@ -0,0 +1,10 @@
+# A ** (double asterisk) sign is an exponentiation (power) operator. 
+# Its left argument is the base, its right, the exponent.
+
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+# Classical mathematics prefers notation with superscripts, just like this: 2^3. 
+# Pure text editors don't accept that, so Python uses ** instead, e.g., 2 ** 3.
