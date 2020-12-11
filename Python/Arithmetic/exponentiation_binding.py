34c741a0ef765738da069317416dd7a3313588d2
Joseph Porr <usmarine1978@gmail.com>
2020-12-10 20:32:08 -0500

    Create exponentiation_binding.py

---
+++ Python/Arithmetic/exponentiation_binding.py
@@ -0,0 +1,16 @@
+
+print(2 ** 2 ** 3)
+
+# the exponentiation operator uses right-sided binding.
+
+# 2 ** 3 → 8; 2 ** 8 → 256
+
+print(2 * 3 % 5)
+
+# 1	+, -	unary
+# 2	**	
+# 3	*, /, %	
+# 4	+, -	binary
+
+print(2 * 3 % 5) 
+#  print(2 * 3) = 6; print(6 % 5) = 1
