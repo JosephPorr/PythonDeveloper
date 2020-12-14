da4b2da1c176cd78e3fc9c5af140157959c57704
Joseph Porr <usmarine1978@gmail.com>
2020-12-13 21:55:47 -0500

    Update variable_+operator.py

--- Python/Variables/variable_+operator.py
+++ Python/Variables/variable_+operator.py
@@ -2,3 +2,11 @@
 
 var = "3.7.1"
 print("Python version: " + var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+# The var variable is created and assigned a value of 100. 
+# Then, the same variable is assigned a new value: 
+# the result of adding 200 to 300, which is 500.
