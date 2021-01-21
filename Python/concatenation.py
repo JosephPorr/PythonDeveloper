d88ca00788b88df913477e0dd2eacb522432aa71
Joseph Porr <usmarine1978@gmail.com>
2021-01-21 17:41:41 -0500

    Create concatenation.py

---
+++ Python/concatenation.py
@@ -0,0 +1,6 @@
+# The + (plus) sign, when applied to two strings, becomes a concatenation operator
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
