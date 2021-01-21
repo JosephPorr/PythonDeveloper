1c336cbf5169a47082d9503ecc885f304c1711f6
Joseph Porr <usmarine1978@gmail.com>
2021-01-20 20:58:41 -0500

    Create hypotenuse.py

---
+++ Python/Arithmetic/hypotenuse.py
@@ -0,0 +1,6 @@
+# able to read the lengths of the legs from the console.
+
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
