2d5c04df1f430dce720e63c837070db3f1fe66ec
Joseph Porr <usmarine1978@gmail.com>
2021-01-21 17:48:04 -0500

    Create replication_operator.py

---
+++ Python/replication_operator.py
@@ -0,0 +1,5 @@
+# The * (asterisk) sign, when applied to a string and number (or a number and string, as it remains commutative in this position) becomes a replication operator:
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
