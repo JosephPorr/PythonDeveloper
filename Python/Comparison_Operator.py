47960d93fa8eba97fdec8279c32218b95fdbefe3
Joseph Porr <usmarine1978@gmail.com>
2021-02-19 20:32:55 -0500

    Create Comparison_Operator.py

---
+++ Python/Comparison_Operator.py
@@ -0,0 +1,10 @@
+# becoming familiar with the the input() function;
+# becoming familiar with comparison operators in Python.
+
+#Using one of the comparison operators in Python, 
+# write a simple two-line program that takes the parameter n as input, 
+# which is an integer, and prints False if n is less than 100, 
+# and True if n is greater than or equal to 100.
+
+n = int(input("Enter a value for n: "))
+print(n >= 100)
