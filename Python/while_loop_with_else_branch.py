d845c3bbba3d99269d01719bbf371a11d7cf2fc4
Joseph Porr <usmarine1978@gmail.com>
2021-03-15 21:01:42 -0400

    Create while_loop_with_else_branch.py

---
+++ Python/while_loop_with_else_branch.py
@@ -0,0 +1,9 @@
+# The loop's else branch is always executed once, 
+# regardless of whether the loop has entered its body or not.
+
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
