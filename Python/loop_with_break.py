bbfa4f3d8f680bf2fc2c26123ae615a4cbb98d87
Joseph Porr <usmarine1978@gmail.com>
2021-04-08 11:46:03 -0400

    Update loop_with_break.py

--- Python/loop_with_break.py
+++ Python/loop_with_break.py
@@ -11,5 +11,5 @@ for ch in "email@email.com":  # ch = characters
 # When a comma is used at the end of a PRINT statement, 
 # the data in the next PRINT statement is continued on the same line 
 # beginning in the next print zone. When " " (quote, blank, quote) is used 
-# as a print field the computer will skip to the next print field. Y
-# ou are actually telling the computer to print a blank field.
+# as a print field the computer will skip to the next print field. 
+# You are actually telling the computer to print a blank field.
