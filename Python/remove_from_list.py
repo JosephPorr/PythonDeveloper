52c48df8164d55583986f0d898fa02d63eed48c5
Joseph Porr <usmarine1978@gmail.com>
2021-04-26 20:35:44 -0400

    Update remove_from_list.py

--- Python/remove_from_list.py
+++ Python/remove_from_list.py
@@ -12,12 +12,12 @@ print("\nPrevious list content:", numbers) # printing previous list content
 numbers[1] = numbers[4] # copying value of the fifth element to the second
 print("Previous list content:", numbers) # printing previous list content
 
-print("\nList's length:", len(numbers)) # printing previous list length
+###
+
+del numbers[1] # Removing the seconf element from the list
+print("New list's length:", len(numbers))  # prints the new list
+print("\nNew list content:", numbers) # Printing current list content
 
 ###
 
-del numbers[1] # removing the second element from the list
-print("New list's length:", len(numbers)) # printing new list length
-print("\nNew list content:", numbers) # printing current list content
 
-###
