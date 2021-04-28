ff1a01dff349ab7b41e92de349f5e5986892e185
Joseph Porr <usmarine1978@gmail.com>
2021-04-27 20:02:32 -0400

    Delete negative_indices.py

--- Python/negative_indices.py
+++
@@ -1,7 +0,0 @@
-# An element with an index equal to -1 is the last one on the list
-# An element with an index equal to -2 is the second one on the list
-
-numbers = [111, 7 , 2, 1]
-print("Current list:", numbers)
-print(numbers[-1])
-print(numbers[-2])
