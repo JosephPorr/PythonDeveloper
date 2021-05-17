a22074849e9a99a849ad71cb3198c2ef502671fc
Joseph Porr <usmarine1978@gmail.com>
2021-05-17 19:30:28 -0400

    Create ifelseelif_shorthand.py

---
+++ Python/Loops/ifelseelif_shorthand.py
@@ -0,0 +1,27 @@
+# if any of the if-elif-else branches contains just one instruction, 
+# you may code it in a more comprehensive form 
+# (you don't need to make an indented line after the keyword, 
+# but just continue the line after the colon).
+
+# read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+
+# we check if the second number is larger than current largest_number
+# and update largest_number if needed
+if number2 > largest_number:
+    largest_number = number2
+
+# we check if the third number is larger than current largest_number
+# and update largest_number if needed
+if number3 > largest_number:
+    largest_number = number3
+
+# print the result
+print("The largest number is:", largest_number)
