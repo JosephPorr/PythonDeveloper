251e23c6d83b3779ce317d939bf58f663f49696b
Joseph Porr <usmarine1978@gmail.com>
2021-05-18 12:16:54 -0400

    Create in_not_in_list.py

---
+++ Python/lists/in_not_in_list.py
@@ -0,0 +1,21 @@
+# Two operators able to look through the list in order to check whether a specific value is stored inside the list or not.
+
+# These operators are:
+#   elem in myList
+#   elem not in myList
+
+# The first of them (in) checks if a given element (its left argument) is currently stored somewhere inside the list 
+# (the right argument) - the operator returns True in this case.
+
+# The second (not in) checks if a given element (its left argument) is absent in a list - 
+# the operator returns True in this case.
+
+myList = [0, 3, 12, 8, 2]
+
+print(5 in myList)
+print(5 not in myList)
+print(12 in myList)
+# output
+#   False
+#   True
+#   True
