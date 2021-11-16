a638c72e5c017b76f5f8c630fdf5baebf36a3b6b
Joseph Porr <usmarine1978@gmail.com>
2021-11-16 13:16:47 -0500

    Update main.py

--- PCAP3103/modules/main.py
+++ PCAP3103/modules/main.py
@@ -8,4 +8,24 @@ import helpers
 name = "Joseph Porr"
 print(f"Lowercase Letters: {helpers.extract_lower(name)}")
 print(f"Uppercase Letters: {helpers.extract_upper(name)}")
-                            
+
+###
+
+# Note:  Helpers can be shorten to just an "h".
+import helpers as h
+
+name = "Joseph Porr"
+print(f"Lowercase Letters: {h.extract_lower(name)}")
+print(f"Uppercase Letters: {h.extract_upper(name)}")
+
+###
+
+# Can call functions directly form the module.
+
+from helpers import extract_upper, extract_lower
+
+name = "Joseph Porr"
+print(f"Lowercase Letters: {extract_lower(name)}")
+print(f"Uppercase Letters: {extract_upper(name)}")
+
+#
