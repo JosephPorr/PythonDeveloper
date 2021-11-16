e325379e0a5c9fef4551ce2bd94f386c1a96302e
Joseph Porr <usmarine1978@gmail.com>
2021-11-15 20:26:45 -0500

    Create helpers.py

---
+++ PCAP3103/modules/helpers.py
@@ -0,0 +1,7 @@
+# This function can be called and used multiples times.
+
+def extract_upper(phrase):  # defined a function to extract a uppercase phrase.
+    return list(filter(str.isupper, phrase)) # there's a list, and filter the string with item that is uppercase of the phrase
+
+def extract_lower(phrase):
+    return list(filter(str.islower, phrase))
