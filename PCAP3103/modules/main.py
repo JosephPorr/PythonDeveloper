eed305e40c0a2353a8d5a862d35e22c26d819b3d
Joseph Porr <usmarine1978@gmail.com>
2021-11-15 20:26:55 -0500

    Create main.py

---
+++ PCAP3103/modules/main.py
@@ -0,0 +1,11 @@
+import helpers
+
+# The import module was the fucntion created in the helpers file.
+# It's recommended to run the main.py file with only the mod
+# This would show if there are errors.  
+# If there are no results, then its working properly.
+
+name = "Joseph Porr"
+print(f"Lowercase Letters: {helpers.extract_lower(name)}")
+print(f"Uppercase Letters: {helpers.extract_upper(name)}")
+                            
