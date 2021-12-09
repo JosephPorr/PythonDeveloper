64c27841101fc64b1ec8e9ed7e69d874409b14f9
Joseph Porr <usmarine1978@gmail.com>
2021-12-09 07:36:33 -0500

    commit on master

---
+++ PCAP3103/modules/helpers/src/__init__.py
@@ -0,0 +1,6 @@
+#  __init__ is a package and contents will run 1st.
+# Basically, the modules can be packaged in init and be easily called from elsewhere
+
+# can import functions from specic files
+
+from .strings import *  # The * means import everything.
