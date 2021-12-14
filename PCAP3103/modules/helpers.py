2b25e9efa894bc7e5f1bb8bc06a68ae7137e1620
Joseph Porr <usmarine1978@gmail.com>
2021-12-13 20:32:09 -0500

    Delete helpers.py

--- PCAP3103/modules/helpers.py
+++
@@ -1,9 +0,0 @@
-# This function can be called and used multiples times.
-
-__all__ = "extract_upper" # This means if all is extracted for this file, it will only extract the upper phrase
-
-def extract_upper(phrase):  # defined a function to extract a uppercase phrase.
-    return list(filter(str.isupper, phrase)) # there's a list, and filter the string with item that is uppercase of the phrase
-
-def extract_lower(phrase):
-    return list(filter(str.islower, phrase))
