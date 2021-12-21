f8c61db54647f1aba1a7757b593fb695596e523b
Joseph Porr <usmarine1978@gmail.com>
2021-12-20 20:45:18 -0500

    commit on master

--- PCAP3103/modules/helpers/strings.py
+++ PCAP3103/modules/helpers/strings.py
@@ -1,4 +1,11 @@
 def extract_upper(phrase):
+    """
+    extract_upper takkes a string and returns a list containing
+    only the uppercase characters from the string.
+
+    >>> extract_upper("Hello There, BOB")
+    ['H', 'T', 'B', 'O', '']
+    """
     return list(filter(str.isupper, phrase))
 
 def extract_lower(phrase):
