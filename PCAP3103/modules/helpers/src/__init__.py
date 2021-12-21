f2b35b89386aafa2c500e2a23f72a2808e5cf6e9
Joseph Porr <usmarine1978@gmail.com>
2021-12-20 20:28:56 -0500

    commit on master

--- PCAP3103/modules/helpers/src/__init__.py
+++ PCAP3103/modules/helpers/src/__init__.py
@@ -2,6 +2,12 @@
 # Basically, the modules can be packaged in init and be easily called from elsewhere
 
 # can import functions from specic files
+
+"""
+Helpers is a package that provides easy to use helper functions
+and variables.
+"""
+
 __all__ = ['extract_upper']
 
 from .strings import *  # The * means import everything.
