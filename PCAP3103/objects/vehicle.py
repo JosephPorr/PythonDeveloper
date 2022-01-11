dd870c6e51206ee4f0bc54cd3c642afd067c52e8
Joseph Porr <usmarine1978@gmail.com>
2022-01-10 20:22:08 -0500

    commit on master

---
+++ PCAP3103/objects/vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:   #Class is a clasifictation of an object.Vehicle is the type.
+    """
+    Vehicle is a means of transportation
+    """
+
+    def _init_(self):
+        """
+        Customizes the initialization of the object
+        """
+        pass
