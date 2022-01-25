3a9fff1871a8978aa0fa6839c6f6880eb9940247
Joseph Porr <usmarine1978@gmail.com>
2022-01-24 19:23:56 -0500

    commit on master

--- PCAP3103/objects/vehicle.py
+++ PCAP3103/objects/vehicle.py
@@ -3,8 +3,19 @@ class Vehicle:   #Class is a clasifictation of an object.Vehicle is the type.
     Vehicle is a means of transportation
     """
 
-    def _init_(self):
-        """
-        Customizes the initialization of the object
-        """
-        pass
+    class_variable = "Joe"
+    # Self, customizes the initialization of the object. In this case, Vehicle.
+    def __init__(self, engine, tires):  # Methods are fuctions attached to an object
+        self.engine = engine
+        self.tires = tires
+        
+    @classmethod  #converts a function to a class method.
+    def bicycle(cls, tires=None):
+        if not tires:
+            tires = [cls.default_tire, cls.default_tire]
+        return cls(None, tires)
+
+    def description(self):
+        print(f"A vehicle with an {self.engine} engine, and {self.tires} tires")
+    
+
