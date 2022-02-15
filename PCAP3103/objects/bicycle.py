4fe092c1a1c2a174ad1ad556e7fe4982f6f6d6f3
Joseph Porr <usmarine1978@gmail.com>
2022-02-14 20:36:13 -0500

    commit on master

---
+++ PCAP3103/objects/bicycle.py
@@ -0,0 +1,22 @@
+#  Instead re=writing the same code, this will inherit the classmethod "vehicle."
+
+from vehicle import Vehicle
+
+class Bicycle(Vehicle):  #this will directly inherit the classmethod.
+    default_tire = 'tire'
+
+# the Vehicle class requuired two positions, 'engine' & 'tires'.
+
+    def __init__(self, tires=None, distance_traveled=0, unit='miles'):
+        super().__init__(distance_traveled, unit)
+        if not tires:
+            tires = [self.default_tire,self.default.tire]
+        self.tires = tires
+
+    def description(self):
+        initial = super().description()
+        return f"{initial} on {len(self.tires)} tires"
+
+if __name__ == "__main__":
+    bike = Bicycle()
+    print(bike.drescription())
--- PCAP3103/objects/vehicle.py
+++ PCAP3103/objects/vehicle.py
@@ -1,3 +1,8 @@
+# classmethod means: when this method is called, 
+# we pass the class as the first argument instead of the instance of that class (as we normally do with methods).
+# classmethod() methods are bound to a class rather than an object. 
+# Class methods can be called by both class and object 
+
 class Vehicle:   #Class is a clasifictation of an object.Vehicle is the type.
     """
     Vehicle is a means of transportation
@@ -5,17 +10,18 @@ class Vehicle:   #Class is a clasifictation of an object.Vehicle is the type.
 
     class_variable = "Joe"
     # Self, customizes the initialization of the object. In this case, Vehicle.
-    def __init__(self, engine, tires):  # Methods are fuctions attached to an object
-        self.engine = engine
-        self.tires = tires
+    def __init__(self, distance_traveled=0, unit='miles'):  # Methods are fuctions attached to an object
+        self.distance_traveled = distance_traveled
+        self.unit = unit
         
-    @classmethod  #converts a function to a class method.
-    def bicycle(cls, tires=None):
-        if not tires:
-            tires = [cls.default_tire, cls.default_tire]
-        return cls(None, tires)
+    # @classmethod  #converts a function to a class method.
+    # def bicycle(cls, tires=None):
+    #    if not tires:
+    #        tires = [cls.default_tire, cls.default_tire]
+    #    return cls(None, tires)
 
     def description(self):
-        print(f"A vehicle with an {self.engine} engine, and {self.tires} tires")
-    
+        return (f"A {self.__class__.__name__} that has traveled {self.distance_traveled} {self.unit}")
+    # A print function would end the script, and not work in other classmethods
+    # use return instead.
 
